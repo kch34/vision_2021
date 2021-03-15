64e5562e960c05bab53e3a60d0e0658d333efa08
Kevin <kch34@pitt.edu>
2021-03-14 21:33:10 -0400

    Created new folder path
    I created the new folder path to the whale shark directory.  And began the whale shark cnn.

---
+++ Kevin-whale-shark/whale_shark_cnn.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 21:30:24 2021
+
+@author: Hostl
+"""
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import time
+import os
+import copy
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from multiprocessing import Process, freeze_support
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from sklearn.metrics import confusion_matrix
+import time
+
+if __name__ == '__main__':
+    start_time = time.time()
+    freeze_support()
+    print("hi")
